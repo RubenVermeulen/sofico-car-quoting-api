342a7f6e138d5050a702089c103cb85cdda1539c
Ruben Vermeulen <hello@rubenvermeulen.be>
2019-02-12 19:38:34 +0100

    updates package name and port assignment

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
-import { environment } from './environment/environment';
 
 // app
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3010);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
